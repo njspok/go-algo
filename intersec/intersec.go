285cfbd0b1c80e155b0e984308b20f9fb154f214
Khamov Oleg <njspok@gmail.com>
2025-03-28 18:28:30 +0500

    intersec

--- intersec/intersec.go
+++ intersec/intersec.go
@@ -5,5 +5,20 @@ func Intersec(a, b []int) []int {
 		return []int{}
 	}
 
-	return []int{11}
+	cnt := make(map[int]int)
+
+	for _, v := range a {
+		cnt[v]++
+	}
+
+	res := make([]int, 0)
+
+	for _, v := range b {
+		if c, ok := cnt[v]; ok && c > 0 {
+			cnt[v]--
+			res = append(res, v)
+		}
+	}
+
+	return res
 }
