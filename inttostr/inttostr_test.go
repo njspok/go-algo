3e21416f2ca3ba917c7fdd2281c724e4072434b5
Khamov Oleg <njspok@gmail.com>
2025-04-11 19:42:42 +0500

    leetcode Count Symmetric Integers

--- inttostr/inttostr_test.go
+++ inttostr/inttostr_test.go
@@ -12,16 +12,14 @@ func Test(t *testing.T) {
 		0,
 		1, 2, 3, 4, 5, 6, 7, 8, 9,
 		11, 12,
-		123,
+		100, 123,
 		math.MaxInt,
 		-1, -2, -3, -4, -5, -6, -7, -8, -9,
 		-11, -12,
-		-123,
+		-100, -123,
 		math.MinInt,
 	}
 	for _, n := range tests {
-		t.Run(strconv.Itoa(n), func(t *testing.T) {
-			require.Equal(t, strconv.Itoa(n), IntToSTr(n))
-		})
+		require.Equal(t, strconv.Itoa(n), IntToSTr(n))
 	}
 }
