0d4800a70a8d69478abae7f5a79e98d753d29298
Khamov Oleg <njspok@gmail.com>
2025-05-30 18:46:53 +0500

    leetcode remove duplicates

---
+++ leetcode/removeduplicates/removeduplicates_test.go
@@ -0,0 +1,57 @@
+package removeduplicates
+
+import (
+	"fmt"
+	"github.com/stretchr/testify/require"
+	"sort"
+	"testing"
+)
+
+func removeDuplicates(nums []int) int {
+	if len(nums) == 0 {
+		return 0
+	}
+
+	unique := make(map[int]struct{})
+
+	for _, v := range nums {
+		if _, ok := unique[v]; !ok {
+			unique[v] = struct{}{}
+			continue
+		}
+	}
+
+	var res []int
+	for k := range unique {
+		res = append(res, k)
+	}
+	sort.Slice(res, func(i, j int) bool { return res[i] < res[j] })
+
+	for n, v := range res {
+		nums[n] = v
+	}
+
+	return len(res)
+}
+
+func Test(t *testing.T) {
+	tests := []struct {
+		nums []int
+		want []int
+		k    int
+	}{
+		{nums: []int{}, want: []int{}, k: 0},
+		{nums: []int{1, 1, 1, 2, 2, 2, 3}, want: []int{1, 2, 3}, k: 3},
+		{nums: []int{0, 0, 1, 1, 1, 2, 2, 3, 3, 4}, want: []int{0, 1, 2, 3, 4}, k: 5},
+	}
+	for _, test := range tests {
+		t.Run(fmt.Sprintf("%v", test.nums), func(t *testing.T) {
+			k := removeDuplicates(test.nums)
+			require.Equal(t, test.k, k)
+
+			for i := 0; i < test.k; i++ {
+				require.Equal(t, test.nums[i], test.want[i], test.nums)
+			}
+		})
+	}
+}
