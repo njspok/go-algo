254ec3897e400963b5726dd09fa84e505ed0af49
Khamov Oleg <njspok@gmail.com>
2025-07-10 19:41:54 +0500

    leetcode remove duplicates

--- leetcode/firstoccurrence/firstoccurrence.go
+++ leetcode/firstoccurrence/firstoccurrence.go
@@ -9,12 +9,24 @@ func strStr(haystack string, needle string) int {
 		if i+len(needle) > len(haystack) {
 			return -1
 		}
-		if haystack[i:i+len(needle)] != needle {
-			i += len(needle)
+
+		if d := diff(haystack[i:i+len(needle)], needle); d >= 0 {
+			i += d + 1
 			continue
 		}
 		return i
 	}
 
+	return -1
+}
+
+// >=0 position where str1 diff str2
+// -1 if str1 == str2
+func diff(str1, str2 string) int {
+	for i := range str1 {
+		if str1[i] != str2[i] {
+			return i
+		}
+	}
 	return -1
 }
