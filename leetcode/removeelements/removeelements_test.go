76f391d6703768e3ddfe35630437f3abda8be787
Khamov Oleg <njspok@gmail.com>
2025-07-29 17:51:58 +0500

    leetcode removeelements

--- leetcode/removeelements/removeelements_test.go
+++ leetcode/removeelements/removeelements_test.go
@@ -1,22 +1,22 @@
 package removeelements
 
 import (
+	"fmt"
 	"github.com/stretchr/testify/require"
 	"slices"
 	"sort"
-	"strconv"
 	"testing"
 )
 
 func removeElement(nums []int, val int) int {
-
-	for i := range nums {
+	i := 0
+	for i < len(nums) {
 		if nums[i] != val {
+			i++
 			continue
 		}
 
 		nums = append(nums[:i], nums[i+1:]...)
-		break
 	}
 
 	return len(nums)
@@ -34,9 +34,12 @@ func Test(t *testing.T) {
 		{nums: []int{5, 4, 3, 2, 1}, val: -1, act: []int{1, 2, 3, 4, 5}, k: 5},
 		{nums: []int{1, 2, 3, 4, 5}, val: 1, act: []int{2, 3, 4, 5}, k: 4},
 		{nums: []int{1, 2, 3, 4, 5}, val: 5, act: []int{1, 2, 3, 4}, k: 4},
+		{nums: []int{1, 1, 2, 3, 4}, val: 1, act: []int{2, 3, 4}, k: 3},
+		{nums: []int{1, 2, 3, 4, 2}, val: 2, act: []int{1, 3, 4}, k: 3},
+		{nums: []int{0, 1, 2, 2, 3, 0, 4, 2}, val: 2, act: []int{0, 0, 1, 3, 4}, k: 5},
 	}
-	for n, test := range tests {
-		t.Run(strconv.Itoa(n), func(t *testing.T) {
+	for _, test := range tests {
+		t.Run(fmt.Sprintf("%v", test.nums), func(t *testing.T) {
 			nums := slices.Clone(test.nums)
 			k := removeElement(nums, test.val)
 			act := make([]int, 0, k)
