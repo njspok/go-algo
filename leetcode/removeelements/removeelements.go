3c8d11ca8aa637919cd2f11b53c67c0ae3f482d0
Khamov Oleg <njspok@gmail.com>
2025-07-30 10:05:42 +0500

    leetcode removeelements

--- leetcode/removeelements/removeelements.go
+++ leetcode/removeelements/removeelements.go
@@ -1,1 +1,27 @@
 package removeelements
+
+func removeElement(nums []int, val int) int {
+	var k int
+	for i := 0; i < len(nums); i++ {
+		nums[k] = nums[i]
+
+		if nums[i] != val {
+			k++
+		}
+	}
+	return k
+}
+
+func removeElementTrivial(nums []int, val int) int {
+	i := 0
+	for i < len(nums) {
+		if nums[i] != val {
+			i++
+			continue
+		}
+
+		nums = append(nums[:i], nums[i+1:]...)
+	}
+
+	return len(nums)
+}
