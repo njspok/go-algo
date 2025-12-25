57c6c7f81897512f9822539b032681bbb70bdd93
Khamov Oleg <njspok@gmail.com>
2025-12-25 10:48:34 +0500

    Best Time to Buy and Sell Stock

--- leetcode/besttimetoby/besttimetoby_test.go
+++ leetcode/besttimetoby/besttimetoby_test.go
@@ -3,44 +3,84 @@ package besttimetoby
 import (
 	"fmt"
 	"github.com/stretchr/testify/require"
+	"math/rand"
 	"testing"
 )
 
 // Complexity
 // - memory O(1)
-// - time O(n*n)
+// - time O(n)
 func maxProfit(prices []int) int {
 	if len(prices) == 0 {
 		return 0
 	}
 
-	res := 0
+	minPrice := prices[0]
+	maxProfit := 0
+
+	for _, price := range prices[1:] {
+		if price < minPrice {
+			minPrice = price
+			continue
+		}
+
+		profit := price - minPrice
+		if profit > maxProfit {
+			maxProfit = profit
+		}
+	}
+
+	return maxProfit
+}
+
+// Complexity
+// - memory O(1)
+// - time O(n*n)
+func maxProfitTrivial(prices []int) int {
+	if len(prices) == 0 {
+		return 0
+	}
+
+	maxProfit := 0
 	for i, pbuy := range prices[:len(prices)-1] {
 		for _, psel := range prices[i+1:] {
 			profit := psel - pbuy
-			if res < profit {
-				res = profit
+			if maxProfit < profit {
+				maxProfit = profit
 			}
 		}
 	}
 
-	return res
+	return maxProfit
 }
 
 func Test(t *testing.T) {
-	tests := []struct {
-		prices []int
-		result int
-	}{
-		{nil, 0},
-		{[]int{}, 0},
-		{[]int{7, 1, 5, 3, 6, 4}, 5},
-		{[]int{7, 1, 5, 3, 6, 4, 1, 10, 2, 9}, 9},
-		{[]int{7, 6, 4, 3, 1}, 0},
-	}
-	for _, test := range tests {
-		t.Run(fmt.Sprintf("%+v", test.prices), func(t *testing.T) {
-			require.Equal(t, test.result, maxProfit(test.prices))
-		})
-	}
+	t.Run("fixed", func(t *testing.T) {
+		tests := []struct {
+			prices []int
+			result int
+		}{
+			{nil, 0},
+			{[]int{}, 0},
+			{[]int{7, 1, 5, 3, 6, 4}, 5},
+			{[]int{7, 1, 5, 3, 6, 4, 1, 10, 2, 9}, 9},
+			{[]int{7, 6, 4, 3, 1}, 0},
+		}
+		for _, test := range tests {
+			t.Run(fmt.Sprintf("%+v", test.prices), func(t *testing.T) {
+				require.Equal(t, test.result, maxProfit(test.prices))
+			})
+		}
+	})
+	t.Run("compared", func(t *testing.T) {
+		for range 100 {
+			var prices []int
+			for range 100 {
+				num := rand.Intn(100)
+				prices = append(prices, num)
+			}
+
+			require.Equal(t, maxProfitTrivial(prices), maxProfit(prices))
+		}
+	})
 }
