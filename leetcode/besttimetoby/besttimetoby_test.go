79f117efce78e4c48999bb6f8c8f56affc9a6f13
Khamov Oleg <njspok@gmail.com>
2025-12-25 08:45:49 +0500

    Best Time to Buy and Sell Stock

--- leetcode/besttimetoby/besttimetoby_test.go
+++ leetcode/besttimetoby/besttimetoby_test.go
@@ -6,8 +6,25 @@ import (
 	"testing"
 )
 
+// Complexity
+// - memory O(1)
+// - time O(n*n)
 func maxProfit(prices []int) int {
-	return 0
+	if len(prices) == 0 {
+		return 0
+	}
+
+	res := 0
+	for i, pbuy := range prices[:len(prices)-1] {
+		for _, psel := range prices[i+1:] {
+			profit := psel - pbuy
+			if res < profit {
+				res = profit
+			}
+		}
+	}
+
+	return res
 }
 
 func Test(t *testing.T) {
@@ -17,6 +34,9 @@ func Test(t *testing.T) {
 	}{
 		{nil, 0},
 		{[]int{}, 0},
+		{[]int{7, 1, 5, 3, 6, 4}, 5},
+		{[]int{7, 1, 5, 3, 6, 4, 1, 10, 2, 9}, 9},
+		{[]int{7, 6, 4, 3, 1}, 0},
 	}
 	for _, test := range tests {
 		t.Run(fmt.Sprintf("%+v", test.prices), func(t *testing.T) {
